1f28ed31eae5a0a9f0b3dfccc59ace7e8bde3cfb
Fadil Hisyam <ichorvfx@gmail.com>
2024-05-10 21:15:09 +0700

    asdasdaw

--- db.sql
+++ db.sql
@@ -26,14 +26,16 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `post`
 --
+DROP TABLE IF EXISTS `post`
 
 CREATE TABLE `post` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8,
   `caption` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `brand` varchar(50) NOT NULL,
   `release_date` date NOT NULL,
-  `filename` varchar(255) NOT NULL
+  `filename` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -46,27 +48,6 @@ INSERT INTO `post` (`id`, `caption`, `description`, `brand`, `release_date`, `fi
 (3, 'Kamen Rider Wizard Flame', 'The figure has a black helmet with orange lining and a yellow visor on the right side and a red visor on the left side. The chest plate is orange with yellow accents and a red lightning bolt symbol. The right arm is orange and the left arm is red.  It has a black belt with a silver buckle in the center.', 'Bandai S.H.Figuarts', '2013-04-02', '31.jpeg'),
 (6, 'Kamen Rider Faiz', 'The figure is a close-up of the head of a Kamen Rider Faiz S.H.Figuarts action figure.  Kamen Rider Faiz is a character from the 2003-2004 Japanese television series Kamen Rider Faiz. The figure has a red head with a clear visor that has a red light-up feature.  The figure also has a black mouth guard and silver accents around the eyes.', 'Bandai S.H.Figuarts', '2003-08-26', '6.jpeg'),
 (7, 'Kamen Rider Dark Kabuto', 'The figure is a close-up of the upper torso of a Dark Kabuto action figure. Dark Kabuto is a character from the 2006-2007 Japanese television series Kamen Rider Kabuto. The figure has a black helmet with a horn-like protrusions on either side. The eyes glow red and there’s a silver mouthpiece covering the mouth. The chest plate is black with yellow trim and a green Kabuto虫 (Kabuto) insect symbol in the center.', 'Bandai Figuarts ZERO', '2006-09-13', '43.jpeg');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `post`
---
-ALTER TABLE `post`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `post`
---
-ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
